9c22356593691a751b7492ec4e19969d730d799c
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2024-04-24 23:23:06 +0300

    upated shop page

--- src/componenets/Shop.jsx
+++ src/componenets/Shop.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../styles/Shop.css'
-import shirt1 from '../assets/shirt1.jpeg'
-import cap1 from '../assets/cap1.png'
-import hoodie1 from '../assets/hoodie1.png'
+import shirt1 from '../assets/BlackT.png'
+import shirt2 from '../assets/whiteT.png'
+import cap1 from '../assets/BlackMug.png'
+import hoodie1 from '../assets/wh.png'
+import hoodie2 from '../assets/bh.png'
+
 import Title from "./Title"
 import Navbar from './Navbar'
 import Footer from './Footer'
@@ -21,32 +24,32 @@ const Shop = () => {
             <div className="product-item">
               
                 <img className="product-image" src={shirt1} alt="Thinking T-shirt" />
-                <h2>Thinking T-shirt</h2>
+                <h2>Black T-shirt</h2>
                 <p>High-quality cotton T-shirt for deep thinkers.</p>
                 <button className="buy-button">Buy Now</button>
             </div>
             <div className="product-item">
                 <img className="product-image" src={cap1} alt="Thinking Cap" />
-                <h2>Thinking Cap</h2>
-                <p>Stylish cap for thoughtful minds.</p>
+                <h2>Coffee Mug</h2>
+                <p>Coffee mug when designing.</p>
                 <button className="buy-button">Buy Now</button>
             </div>
             <div className="product-item">
                 <img className="product-image" src={hoodie1} alt="Thinking Hoodie" />
-                <h2>Thinking Hoodie</h2>
+                <h2>White Hoodie</h2>
                 <p>Warm hoodie for contemplative souls.</p>
                 <button className="buy-button">Buy Now</button>
             </div>
             <div className="product-item">
-                <img className="product-image" src={hoodie1} alt="Thinking Hoodie" />
-                <h2>Thinking Hoodie</h2>
+                <img className="product-image" src={hoodie2} alt="Thinking Hoodie" />
+                <h2>Black Hoodie</h2>
                 <p>Warm hoodie for contemplative souls.</p>
                 <button className="buy-button">Buy Now</button>
             </div>
             <div className="product-item">
-                <img className="product-image" src={hoodie1} alt="Thinking Hoodie" />
+                <img className="product-image" src={shirt2} alt="Thinking Hoodie" />
                 <h2>Thinking Hoodie</h2>
-                <p>Warm hoodie for contemplative souls.</p>
+                <p>High-quality cotton T-shirt for deep thinkers.</p>
                 <button className="buy-button">Buy Now</button>
             </div>
             <div className="product-item">
