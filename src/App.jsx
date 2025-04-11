b3245dca104007f81c4fed368c5b144e792fdb19
Sheldon Odhiambo <sheldonodhiuma04@gmail.com>
2025-04-11 14:13:19 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import {Routes, Route} from "react-router-dom"
-import Navbar from "./componenets/Navbar"
-import Home from "./componenets/Home"
-import About from "./componenets/About"
-import Service from "./componenets/Service"
-import Resource from "./componenets/Resource"
-import Contact from "./componenets/Contact"
-import Footer from "./componenets/Footer"
-import Shop  from "./componenets/Shop"
-import Header  from "./componenets/Header"
-
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./componenets/Navbar";
+import Home from "./componenets/Home";
+import About from "./componenets/About";
+import Service from "./componenets/Service";
+import Resource from "./componenets/Resource";
+import Contact from "./componenets/Contact";
+import Footer from "./componenets/Footer";
+import Shop from "./componenets/Shop";
+import Header from "./componenets/Header";
+import Addition from "./componenets/Addition";
+import ScrollButtons from "./componenets/ScrollButtons";
 
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path="/navbar" element={<Navbar/>}/>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/service" element={<Service/>}/>
-      <Route path="/resource" element={<Resource/>}/>
-      <Route path="contact" element={<Contact/>}/>
-      <Route path="footer" element={<Footer/>}/>
-      <Route path="shop" element={<Shop/>}/>
-      <Route path="header" element={<Header/>}/>
-
-
-
       
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/resource" element={<Resource />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/addition" element={<Addition />} />
+      </Routes>
 
-    </Routes>
+      <ScrollButtons /> 
     </>
-  )
+  );
 }
 
-export default App
+export default App;
